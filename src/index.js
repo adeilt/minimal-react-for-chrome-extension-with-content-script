8c6cadf74fe4ff9d9f8d487b2439ae1b41d86aa3
Wil adeilt Langford <adeilt.wsl@gmail.com>
2023-02-26 17:40:17 -0800

    I don't think any of this is necessary.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './router';
-
-export const init = () => {
-  ReactDOM.render(<Router />, document.querySelector('[data-app]'));
-}
-
-init();
