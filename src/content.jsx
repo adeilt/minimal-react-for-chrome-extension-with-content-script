e62f05fd81198838f9129c8d27c02eec28d51b10
Wil adeilt Langford <adeilt.wsl@gmail.com>
2023-02-26 20:39:27 -0800

    Inject sidecar container in content.jsx.

--- src/content.jsx
+++ src/content.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 
 import "./content.css";
 
+container_id = 'sidecar_container';
+
+if(document.getElementById(container_id)){
+ 	document.getElementById(id).parentNode.removeChild(document.getElementById(container_id));
+} else {
+  sidecar_container = document.createElement('div');
+  sidecar_container.id = container_id;
+  document.body.appendChild(sidecar_container);
+}
+
 ReactDOM.render(
   <Sidecar />,
   document.getElementById('sidecar_container')
