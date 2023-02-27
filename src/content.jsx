b127cd57c1db0fc9e48044aff632f6a90b7104bb
Wil adeilt Langford <adeilt.wsl@gmail.com>
2023-02-26 20:51:07 -0800

    Declare variables properly.

--- src/content.jsx
+++ src/content.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 import "./content.css";
 
-container_id = 'sidecar_container';
+var container_id = 'sidecar_container';
 
 if(document.getElementById(container_id)){
  	document.getElementById(id).parentNode.removeChild(document.getElementById(container_id));
 } else {
-  sidecar_container = document.createElement('div');
+  let sidecar_container = document.createElement('div');
   sidecar_container.id = container_id;
   document.body.appendChild(sidecar_container);
 }
